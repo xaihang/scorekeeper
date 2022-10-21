a2aba888649a0992fa08791bc87ef3438a5573b4
xaihang <xaihang12@gmail.com>
2022-10-21 10:33:49 -0500

    added function to player 2 button

--- script.js
+++ script.js
@@ -5,11 +5,17 @@ const p2Display = document.querySelector('#p2Display');
 
 //logic: when click on player's button we want to add +1 to the current score and have that added score show on the span h1; keep track of the current score
 let p1Score = 0;
+let p2Score = 0;
 
 p1Button.addEventListener('click', function () {
     p1Score += 1;
     p1Display.textContent = p1Score;
 
+})
+
+p2Button.addEventListener('click', function () {
+    p2Score += 1;
+    p2Display.textContent = p2Score;
 })
 
 
