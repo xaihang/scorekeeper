3557ae08a9419bce813a2be0e0bafa5fb85718f1
xaihang <xaihang12@gmail.com>
2022-10-21 11:13:00 -0500

    added reset function

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
 const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
+const resetButton = document.querySelector('#reset');
 
 
 
@@ -31,5 +32,16 @@ p2Button.addEventListener('click', function () {
     }
 
 })
+
+//reset button function:
+resetButton.addEventListener('click', function () {
+    isGameOver = false;
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+
+})
+
 
 
