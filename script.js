09c5ffdb2932ae9870d78c3f0021969cc500a395
xaihang <xaihang12@gmail.com>
2022-10-21 10:52:55 -0500

    added boolean isGameOver var

--- script.js
+++ script.js
@@ -9,17 +9,24 @@ const p2Display = document.querySelector('#p2Display');
 let p1Score = 0;
 let p2Score = 0;
 let winningScore = 5;
+let isGameOver = false; //a boolean to keep track of who hit the winningscore first, if hit then game is over
 
 p1Button.addEventListener('click', function () {
-    if (p1Score !== winningScore) {
+    if (!isGameOver) {
         p1Score += 1;
+        if (p1Score === winningScore) {
+            isGameOver = true;
+        }
         p1Display.textContent = p1Score;
     }
 })
 
 p2Button.addEventListener('click', function () {
-    if (p2Score !== winningScore) {
+    if (!isGameOver) {
         p2Score += 1;
+        if (p2Score === winningScore) {
+            isGameOver = true;
+        }
         p2Display.textContent = p2Score;
     }
 
